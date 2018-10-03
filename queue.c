673961b5815d4759f77353c5fa1771d79f8b170d
yichung <ql3895619@gmail.com>
2018-10-04 04:56:15 +0800

    Add feature: q_new()

--- queue.c
+++ queue.c
@@ -26,7 +26,13 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q==NULL)
+      return NULL
+    
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+    
     return q;
 }
 
